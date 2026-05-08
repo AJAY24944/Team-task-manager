ef9b5e8d7677bbd892dd348cf9a1faa1a1d36239
AJAY24944 <ellandulajaynani@gmail.com>
2026-05-08 23:47:20 +0530

    Final working full-stack task manager

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,20 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-});
-
-API.interceptors.request.use((req) => {
-
-  const token = localStorage.getItem("token");
-
-  if (token) {
-
-    req.headers.Authorization =
-      `Bearer ${token}`;
-  }
-
-  return req;
+    baseURL: "http://localhost:8081/api",
 });
 
 export default API;
