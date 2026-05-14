fe574406b2a101b694510964a441aa88bee72763
AJAY24944 <ellandulajaynani@gmail.com>
2026-05-14 12:09:06 +0530

    Fixed deployed API URL

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:8081/api",
+    baseURL: "https://team-task-manager-production-af7b.up.railway.app/api",
 });
 
 export default API;
